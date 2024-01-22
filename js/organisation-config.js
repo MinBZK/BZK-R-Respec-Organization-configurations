dad769ac96fcda835618dcbbe1baf483ffdb46db
melsk-r <robert.melskens@vng.nl>
2024-01-22 10:46:46 +0100

    Update organisation-config.js

--- js/organisation-config.js
+++ js/organisation-config.js
@@ -22,11 +22,11 @@ var organisationConfig = {
    // Bijv. om de url van de vorige versie niet te tonen, aan begin van de life cycle van een document is deze er nl. nog helemaal niet.
 
    pubDomain: "cim",
-	
+   this: "test",	
    //this: "this", <-- Zo kun je dus eigen variabelen introduceren die je ergens anders kunt gebruiken.
    // Zoals bijv. hier --> 'thisVersion: ["nl_organisationPublishURL", "this", "/", "shortName"],'
 
-   latestVersion: ["nl_organisationPublishURL", "test", "pubDomain", "/", "shortName"],
+   latestVersion: ["nl_organisationPublishURL", "this", "pubDomain", "/", "shortName"],
    // thisVersion: ["nl_organisationPublishURL", "pubDomain", "/", "specStatus", "-", "specType", "-", "shortName", "-", "publishDate"],
    thisVersion: ["nl_organisationPublishURL", "pubDomain", "/", "shortName", "/", "publishVersion"],
    //prevVersion: ["nl_organisationPublishURL", "pubDomain", "/", "previousMaturity", "-", "specType", "-", "shortName", "-", "previousPublishDate"],
