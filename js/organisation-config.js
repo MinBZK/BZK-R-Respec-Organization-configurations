5bdcc09582f53b0ded27e10c7029b390605a24a9
Paul S. <paul.sterkenburg@minBZK.nl>
2024-07-30 19:57:12 +0200

    Update organisation-config.js

--- js/organisation-config.js
+++ js/organisation-config.js
@@ -1,7 +1,7 @@
 var organisationConfig = {
     nl_organisationName: "Min Realisatie",
     nl_organisationStylesURL: "https://gitdocumentatie.logius.nl/publicatie/respec/style/",
-    nl_organisationPublishURL: "https://Min-realisatie.github.io/publicatie",
+    nl_organisationPublishURL: "https://minbzk.github.io/publicatie/",
     // Het hier gedefinieerde logo wordt helemaal bovenaan het Respec document aan de rechterzijde geplaatst.
     // Er is voor gekozen de 'width' property niet te gebruiken waardoor het logo automatisch in de juiste verhoudingen wordt geplaatst.
    // logos: [{
